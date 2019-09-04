cf07d7c1012fb40614ca92a62ebfc77b9b282f69
Daniel <morala84@gmail.com>
2019-09-05 01:30:32 +0200

    Ejercicio 2 parte 1. Consiste en contar las mayusculas de un texto escrito por el usuario. Tiene fallos.

---
+++ ejercicio2.py
@@ -0,0 +1,11 @@
+
+frase_usuario = input("Introduce un texto: ")
+mayusculas = 0
+simbolos = [" ", ".", ","]
+no_simbolo = 0
+for letra in frase_usuario:
+    if letra is simbolos:
+        no_simbolo +=1
+    if letra == letra.upper():
+        mayusculas += 1
+print("Hay {} mayusculas". format(mayusculas))
